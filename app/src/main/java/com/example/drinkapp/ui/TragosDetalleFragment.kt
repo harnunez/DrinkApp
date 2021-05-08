a80fd22f1b5fd854183ceb9264939c0aa072fab0
unknown <harnunez@gmail.com>
2021-05-08 20:06:11 -0300

    Se finalizo con el detalle del trago

--- app/src/main/java/com/example/drinkapp/ui/TragosDetalleFragment.kt
+++ app/src/main/java/com/example/drinkapp/ui/TragosDetalleFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.drinkapp.R
 import com.example.drinkapp.data.model.Drink
+import kotlinx.android.synthetic.main.fragment_tragos_detalle.*
 
 class TragosDetalleFragment : Fragment() {
 
@@ -25,6 +27,16 @@ class TragosDetalleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tragos_detalle, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        drink.let {
+            Glide.with(requireContext()).load(drink.imagen).into(img_trago_detalle)
+            trago_title_detalle.text = drink.nombre
+            trago_desc_detalle.text = drink.descripcion
+        }
     }
 
 }
